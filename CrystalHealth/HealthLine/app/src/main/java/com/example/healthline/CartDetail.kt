a09d8a891169f6a076e649d823a7dcf425e578f3
Nhlamulo-hl <105660273+Nhlamulo-hl@users.noreply.github.com>
2023-11-17 17:52:02 +0200

    Delete CrystalHealth/HealthLine directory

--- CrystalHealth/HealthLine/app/src/main/java/com/example/healthline/CartDetail.kt
+++
@@ -1,3 +0,0 @@
-package com.example.healthline
-
-data class CartDetail(var ItemPrice: String?= null,var Quantity: String?= null)
--- CrystalHealth/HealthLine/app/src/main/java/com/example/healthline/PatientConfirm.kt
+++
@@ -1,3 +0,0 @@
-package com.example.healthline
-
-data class PatientConfirm(var Date: String?= null,var Name: String?= null, var Appoint :String ? =null, var Number: String?= null)
